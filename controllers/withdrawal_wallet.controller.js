88faf1a9a3efcde6655ea58393e6ec5ecef78551
ShraddhaMulekar <mulekarshraddha@gmail.com>
2025-12-02 22:19:16 +0530

    withdrawal from wallet functionality done

---
+++ controllers/withdrawal_wallet.controller.js
@@ -0,0 +1,25 @@
+import { UserModel } from "../models/user.model.js";
+
+export const withdrawalWalletController = async (req, res) => {
+    
+    try {
+      const { amount } = req.body;
+    const user = await UserModel.findById(req.userId);
+    if (user.wallet < amount) {
+      return res
+        .status(400)
+        .json({ message: "Insufficient funds in withdrawal wallet" });
+    }
+    user.wallet -= amount;
+    await user.save();
+    console.log(user.wallet)
+    return res.status(200).json({
+      message: "Withdrawal successful",
+      withdrawal_wallet: user.wallet,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Server error", error: error.message });
+  }
+};
--- routes/wallet.route.js
+++ routes/wallet.route.js
@@ -1,7 +1,9 @@
-import express from "express"
-import { addWalletController } from "../controllers/add_wallet.controller.js"
-import { authMiddleware } from "../middlewares/auth.middleware.js"
+import express from "express";
+import { addWalletController } from "../controllers/add_wallet.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { withdrawalWalletController } from "../controllers/withdrawal_wallet.controller.js";
 
-export const walletRouter = express.Router()
+export const walletRouter = express.Router();
 
-walletRouter.post("/add",authMiddleware, addWalletController)
+walletRouter.post("/add", authMiddleware, addWalletController);
+walletRouter.post("/withdrawal", authMiddleware, withdrawalWalletController);
