cfc31f1f392b4ac1ac97e40534b7ae3a7bb13877
ShraddhaMulekar <mulekarshraddha@gmail.com>
2025-12-02 17:18:26 +0530

    log in functionality done

---
+++ controllers/login.controller.js
@@ -0,0 +1,32 @@
+import { UserModel } from "../models/user.model.js";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const matchEmail = await UserModel.findOne({ email });
+
+    if (!matchEmail) {
+      return res
+        .status(404)
+        .json({ msg: "you are not register. please register!" });
+    } else {
+      bcrypt.compare(password, matchEmail.password, async (err, result)=>{
+        if(result){
+            const payload = {
+                userId : matchEmail._id,
+                userName : matchEmail.name
+            }
+            const token = jwt.sign(payload, process.env.JWT_SECRET)
+            return res.status(200).json({msg: "log in successful!", token});
+        } else{
+            return res.status(404).json({msg: "password invalid!", err});
+        }
+      });
+    }
+  } catch (error) {
+    return res.status(404).json({ msg: "Error in backend user log in post router", error });
+  }
+};
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,10 @@
 import express from "express"
 import { registerController } from "../controllers/register.controller.js"
+import { loginController } from "../controllers/login.controller.js"
 
 const authRouter = express.Router()
 
 authRouter.post("/register", registerController)
+authRouter.post("/login", loginController)
 
 export { authRouter }
