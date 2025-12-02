966132d7f4f622f754e83dac97efb5f0c29e8182
ShraddhaMulekar <mulekarshraddha@gmail.com>
2025-12-02 23:56:41 +0530

    symbol remove from watchlist

---
+++ controllers/remove_watchlist.controller.js
@@ -0,0 +1,24 @@
+import { UserModel } from "../models/user.model.js";
+
+export const removeWatchlistController = async (req, res) => {
+  try {
+    const { symbol } = req.body;
+
+    const user = await UserModel.findById(req.userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    user.watchlist = user.watchlist.filter((item) => item !== symbol);
+    await user.save();
+    return res
+      .status(200)
+      .json({
+        message: "Symbol removed from watchlist",
+        watchlist: user.watchlist,
+      });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+};
--- routes/watchList.route.js
+++ routes/watchList.route.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { addWatchListController } from "../controllers/add_watchList.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { removeWatchlistController } from "../controllers/remove_watchlist.controller.js";
 
 export const watchListRouter = express.Router();
 
 watchListRouter.post("/add", authMiddleware, addWatchListController );
+watchListRouter.post("/remove", authMiddleware, removeWatchlistController)
