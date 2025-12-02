e37613d9008c910a451d2c2614834a7302ca5e19
ShraddhaMulekar <mulekarshraddha@gmail.com>
2025-12-03 00:13:00 +0530

    check all watchlist

---
+++ controllers/check_allWatchlist.controller.js
@@ -0,0 +1,15 @@
+import { UserModel } from "../models/user.model.js"
+
+export const checkAllWatchlistController = async(req, res) => {
+    try {
+        const userId = req.userId
+        const user = await UserModel.findById(userId).select("watchlist")
+
+        if(!user){
+            return res.status(404).json({message: "User not found"})
+        }
+        return res.status(200).json({watchlist: user.watchlist})
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+}
--- routes/watchList.route.js
+++ routes/watchList.route.js
@@ -2,8 +2,10 @@ import express from "express";
 import { addWatchListController } from "../controllers/add_watchList.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { removeWatchlistController } from "../controllers/remove_watchlist.controller.js";
+import { checkAllWatchlistController } from "../controllers/check_allWatchlist.controller.js";
 
 export const watchListRouter = express.Router();
 
 watchListRouter.post("/add", authMiddleware, addWatchListController );
 watchListRouter.post("/remove", authMiddleware, removeWatchlistController)
+watchListRouter.get("/all", authMiddleware, checkAllWatchlistController)
