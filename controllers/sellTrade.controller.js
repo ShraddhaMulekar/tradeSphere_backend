d2789cc82bb595e476c135038706465466a3d180
ShraddhaMulekar <mulekarshraddha@gmail.com>
2025-12-03 19:32:41 +0530

    sell trade functionality done

---
+++ controllers/sellTrade.controller.js
@@ -0,0 +1,39 @@
+import { OrderModel } from "../models/order.model.js";
+import { PortfolioModel } from "../models/portfolio.model.js";
+import { UserModel } from "../models/user.model.js";
+
+export const sellTradeController = async (req, res) => {
+  try {
+    const { symbol, price, quantity } = req.body;
+
+    const stock = await PortfolioModel.findOne({ userId: req.userId, symbol });
+
+    console.log("stock:", stock);
+    if (!stock || stock.quantity < quantity) {
+      return res.status(400).json({ message: "You don't own enough stock" });
+    }
+
+    const totalSellValue = price * quantity;
+
+    stock.quantity -= quantity;
+
+    if (stock.quantity === 0) await stock.deleteOne();
+    else await stock.save();
+
+    const user = await UserModel.findById(req.userId);
+    user.wallet += totalSellValue;
+    await user.save();
+
+    await OrderModel.create({
+      userId: req.userId,
+      symbol,
+      type: "SELL",
+      price,
+      quantity,
+    });
+
+    return res.status(200).json({ message: "Stock sold successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
--- routes/trade.route.js
+++ routes/trade.route.js
@@ -1,7 +1,9 @@
 import express from "express"
 import { buyController } from "../controllers/buyTrade.controller.js"
 import { authMiddleware } from "../middlewares/auth.middleware.js"
+import { sellTradeController } from "../controllers/sellTrade.controller.js"
 
 export const tradeRouter = express.Router()
 
 tradeRouter.post("/buy",authMiddleware, buyController)
+tradeRouter.post("/sell",authMiddleware, sellTradeController)
