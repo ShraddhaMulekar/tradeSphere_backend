6276fa75f96b0095fd15fd961d0dbdfbf876d5fc
ShraddhaMulekar <mulekarshraddha@gmail.com>
2025-12-09 14:04:59 +0530

    Fix: Resolve case-sensitive import issues

--- routes/price.route.js
+++ routes/price.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { priceController } from '../controllers/price.controller.js';
 import { authMiddleware } from '../middlewares/auth.middleware.js';
+import { priceController } from '../controllers/price.controller.js';
 
 export const priceRouter = express.Router();
 
--- routes/stockSearch.route.js
+++ routes/stockSearch.route.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import { authMiddleware } from '../middlewares/auth.middleware.js';
 import { searchStockController } from '../controllers/searchStockController.js';
-import { stockController } from '../controllers/stock.Controller.js';
+import { stockController } from '../controllers/stock.controller.js';
 
 export const stockSearchRouter = express.Router();
 
-stockSearchRouter.get('/search/:symbol', searchStockController);
-stockSearchRouter.get("/quote/:symbol", authMiddleware, stockController);
+stockSearchRouter.get("/search/:symbol", searchStockController)
+stockSearchRouter.get("/quote/:symbol", authMiddleware, stockController)
